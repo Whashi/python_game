67bd8dca4bdef272ab001713450f2acd822e04c3
Whashi <whashi88@gmail.com>
2025-01-19 18:59:39 -0700

    Added a high score feature and removed the game over feature

--- score_board.py
+++ score_board.py
@@ -1,5 +1,7 @@
 from turtle import Turtle
 
+FONT = ("Arial", 12, "bold")
+
 class ScoreBoard(Turtle):
 
     def __init__(self):
@@ -11,18 +13,26 @@ class ScoreBoard(Turtle):
         self.penup()
         self.speed(0)
         self.goto(0, 280)
+        self.get_high_score()
         self.update_score()
 
+    def get_high_score(self):
+        with open("data.txt") as f:
+            self.high_score = f.read()
+            return self.high_score
+
     def update_score(self):
+        self.get_high_score()
         self.clear()
-        self.write(f"Score: {self.score} High Score: {self.high_score}", align="center", font=("Arial", 12, "bold"))
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align="center", font=FONT)
 
     def add_point(self):
         self.score += 1
         self.update_score()
 
     def reset(self):
-        if self.score > self.high_score:
-            self.high_score = self.score
+        if self.score > int(self.high_score):
+            with open("data.txt", mode="w") as f:
+                f.write(str(self.score))
         self.score = 0
         self.update_score()
